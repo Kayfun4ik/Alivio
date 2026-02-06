833698bd14be895201188f6d2b36dc0414056e65
Kayfun4ik <ibragimov.farxad@gmail.com>
2026-02-06 14:42:58 +0400

    Finish

--- Assets/js/main.js
+++ Assets/js/main.js
@@ -19,8 +19,3 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".toggler").checked = false;
   });
 });
-document.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("listitem4").addEventListener("click", function () {
-    document.querySelector(".toggler").checked = false;
-  });
-});
